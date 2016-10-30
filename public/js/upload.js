9218821b3440942bb5e0acd8ce3db8b0aef16043
Artem Boyur <gboyur@gmail.com>
2016-10-30 11:16:06 +0300

    add jq, add img for dragform

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,15 +1,9 @@
 window.remove = DragDrop('.upload', {
   onDrop: function (files, pos) {
     var names = files.map(function (file) { return file.name });
-    window.alert('onDrop fired! ' + files.length + ' files: ' + names.join(', '));
     window.files = files;
     console.log('onDrop', files);
-    console.log('Dropped at coordinates', pos.x, pos.y);
-  },
-  onDropText: function (text, pos) {
-    window.alert('onDropText fired! ' + text);
-    window.text = text;
-    console.log('onDropText', text);
-    console.log('Dropped at coordinates', pos.x, pos.y);
+
+    $('.upload').append('<img id="theImg" src="' + files[0].fullPath +'" />');
   }
 });
